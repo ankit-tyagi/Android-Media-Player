e843d374abcb7e9b092222ff4c793cb1f174bab3
Ankit Tyagi <ankittyaagi@gmail.com>
2017-08-19 11:19:48 +0530

    Model and Adapter work done

--- app/src/main/java/com/ankit/playaudio/MainActivity.java
+++ app/src/main/java/com/ankit/playaudio/MainActivity.java
@@ -2,14 +2,26 @@ package com.ankit.playaudio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<SongInfo> songs = new ArrayList<SongInfo>();
+    RecyclerView recyclerView;
+    SeekBar seekBar;
+    SongAdapter songAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        songAdapter = new SongAdapter(this,songs);
     }
 
 }
--- app/src/main/java/com/ankit/playaudio/SongAdapter.java
+++ app/src/main/java/com/ankit/playaudio/SongAdapter.java
@@ -1,17 +1,33 @@
 package com.ankit.playaudio;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by ankit_000 on 19-08-2017.
  */
 
 public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
+
+    ArrayList<SongInfo> songs;
+    Context context;
+
+    SongAdapter(Context context, ArrayList<SongInfo> songs){
+        this.context = context;
+        this.songs = songs;
+    }
+
     @Override
     public SongHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View myview = LayoutInflater.from(context).inflate(R.layout.row_song,parent,false);
+        return new SongHolder(myview);
     }
 
     @Override
@@ -21,12 +37,17 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
 
     @Override
     public int getItemCount() {
-        return 0;
+        return songs.size();
     }
 
     public class SongHolder extends RecyclerView.ViewHolder {
+        TextView songName, artistName;
+        Button btnAction;
         public SongHolder(View itemView) {
             super(itemView);
+            songName = (TextView) itemView.findViewById(R.id.textViewSongName);
+            artistName = (TextView) itemView.findViewById(R.id.textViewArtistName);
+            btnAction = (Button) itemView.findViewById(R.id.buttonAction);
         }
     }
 }
